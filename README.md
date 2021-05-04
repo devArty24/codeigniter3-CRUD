cd9fe219ad97b2a2bb40d30671651b65a164c24d
devArty24 <ghoul100p@gmail.com>
2021-05-03 19:21:53 -0500

    Backup database

---
+++ README.md
@@ -0,0 +1,21 @@
+# CRUD Codeigniter
+
+* [About Project]
+* [Key Features]
+* [Installation]
+
+## About Project
+
+Code of a basic CRUD, to register doctors in a hospital, users are listed in a table and with paging in addition to a basic login
+
+## Key Features
+
+1. PHP
+2. Codeigniter v3.1.11
+3. Mysql
+4. JavaScript
+5. Jquery
+6. Bootstrap 4
+7. SweetAlert2
+
+## Installation
---
+++ dataBaseBackup/hospital.sql
@@ -0,0 +1,67 @@
+/*
+SQLyog Ultimate v11.11 (64 bit)
+MySQL - 5.5.5-10.1.21-MariaDB : Database - hospital
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*Table structure for table `medicos` */
+
+DROP TABLE IF EXISTS `medicos`;
+
+CREATE TABLE `medicos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(100) DEFAULT NULL,
+  `apellido` varchar(100) DEFAULT NULL,
+  `area` varchar(150) DEFAULT NULL,
+  `cedula` varchar(250) DEFAULT NULL,
+  `especialidad` varchar(250) DEFAULT NULL,
+  `id_usuario` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+/*Data for the table `medicos` */
+
+insert  into `medicos`(`id`,`nombre`,`apellido`,`area`,`cedula`,`especialidad`,`id_usuario`) values (1,'Diego','Rosales','clinica','123456789-12','Medico',11),(2,'Ilse','Garcia','Clinica','121121222-11','Farmasia',6);
+
+/*Table structure for table `migrations` */
+
+DROP TABLE IF EXISTS `migrations`;
+
+CREATE TABLE `migrations` (
+  `version` bigint(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `migrations` */
+
+insert  into `migrations`(`version`) values (1);
+
+/*Table structure for table `usuarios` */
+
+DROP TABLE IF EXISTS `usuarios`;
+
+CREATE TABLE `usuarios` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `nombre_usuario` varchar(100) NOT NULL,
+  `correo` varchar(100) DEFAULT NULL,
+  `contrasena` varchar(100) DEFAULT NULL,
+  `status` tinyint(1) DEFAULT NULL,
+  `range` varchar(10) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
+
+/*Data for the table `usuarios` */
+
+insert  into `usuarios`(`id`,`nombre_usuario`,`correo`,`contrasena`,`status`,`range`) values (1,'adminUser','prueba.registro@gmail.com','123456',1,'admin');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
